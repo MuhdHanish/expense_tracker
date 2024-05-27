3077d11898131ba222b8737274637512d1a59b9e
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-27 14:07:52 +0530

    imported expensesRoute in app and used in /api/expenses

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,15 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 
+// routes
+import { expensesRoute } from "./routes";
+
 const app = new Hono();
 
 app.use(logger());
 
-app.get('/', (c) => c.json({ message: "Hello from Hono!" }));
+app.get("/", (c) => c.json({ message: "Hello from Server!" }));
+
+app.route("/api/expenses", expensesRoute);
 
 export default app;
