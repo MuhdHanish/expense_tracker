426f87d65d887344ab35cc804382369c8983c7d0
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-27 14:00:09 +0530

    added logger middleware

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,9 @@
 import { Hono } from 'hono';
+import { logger } from 'hono/logger';
 
-const app = new Hono()
+const app = new Hono();
+
+app.use(logger());
 
 app.get('/', (c) => c.json({ message: "Hello from Hono!" }));
 
