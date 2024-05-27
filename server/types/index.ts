bb3197800b44e7cd7512ce23ed3ffe0aff0ab3b9
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-27 15:43:31 +0530

    used zod expense schema to creat expense type

--- server/types/index.ts
+++ server/types/index.ts
@@ -1,5 +1,5 @@
-export type TExpense = {
-    id: number;
-    title: string;
-    amount: number;
-};
+import type { z } from "zod";
+import type { expenseSchema } from "../validation";
+
+// zod validation expense schema used to create type of expense
+export type TExpense = z.infer<typeof expenseSchema>;
