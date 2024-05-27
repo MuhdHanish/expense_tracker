fc6dfcfe352362c3261ad1c710cff85c4d25d7db
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-27 20:10:30 +0530

    changed port and host

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 
-const port = process.env.PORT || 8000;
-const hostname = process.env.HOST || "127.0.0.1";
+const port = process.env.PORT || 3000;
+const hostname = process.env.HOST || "0.0.0.0";
 Bun.serve({
     port,
     hostname,
