2a16b7c0a3e19799ab6e358c6df1573a1ef09ae8
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-27 19:49:24 +0530

    folder structure changed pev - ./ ./client, ./server, .gitignore now - ./ ./client, ./server, ./node_modules, .gitingore, .bun.lockb, .packange.json, .tsconfig.json current folder structure help to server the static file if the request someting random instead of hono 404 response changed the serveStatic root and path starting ../ to ./

--- server/app.ts
+++ server/app.ts
@@ -7,12 +7,12 @@ import { expensesRoute } from "./routes";
 
 const app = new Hono();
 
-app.use(logger());
+app.use("*", logger());
 
 app.route("/api/expenses", expensesRoute);
 
 // Serve static files (frontend) for unmatched routes
-app.get("*", serveStatic({ root: "../client/dist" }));
-app.get("*", serveStatic({ path: "../client/dist/index.html" }));
+app.get("*", serveStatic({ root: './client/dist' }));
+app.get("*", serveStatic({ path: './client/dist/index.html' }));
 
 export default app;
