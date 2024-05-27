363c0fa77c433d1e42f31cae4d02afca7c64b0e9
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-27 14:18:02 +0530

    chagned [] to fakeExpenses

--- server/routes/expenses.ts
+++ server/routes/expenses.ts
@@ -1,8 +1,9 @@
 import { Hono } from "hono";
+import { fakeExpense } from "../data";
 
 export const expensesRoute = new Hono()
     .get("/", (c) => {
-        return c.json({ expenses: [] });
+        return c.json({ expenses: fakeExpense });
     })
     .post("/", (c) => {
         return c.json({});
