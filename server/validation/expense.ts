3233278ae55cf9d3a6acc80485349b85af36f6eb
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-27 15:43:12 +0530

    created expense schema using zod and exported

--- server/validation/expense.ts
+++ server/validation/expense.ts
@@ -1,11 +1,15 @@
 import { z } from "zod";
 import { zValidator } from "@hono/zod-validator";
 
-// create expense schema
-const createExpenseSchema = z.object({
+// expense schema
+export const expenseSchema = z.object({
+    id: z.number().int().positive().min(1),
     title: z.string(),
     amount: z.number().positive(),
 });
+
+// create expense schema
+const createExpenseSchema = expenseSchema.omit({ id: true });
 
 // create expense validator
 export const createExpenseValidator = zValidator("json", createExpenseSchema);
