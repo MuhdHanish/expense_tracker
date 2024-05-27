ae871c38e9a8a3fd7d149ad8c51895d13e2599fa
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-27 20:10:50 +0530

    changed host and port in react poxy api traget

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: 'http://127.0.0.1:8000',
+        target: 'http://localhost:3000',
         changeOrigin: true
       }
     }
