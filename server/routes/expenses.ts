79adabbc6a0381cc511d67d3278ae2446be9cd0a
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-27 15:26:36 +0530

    /api/expense/:id get handler added try catch block added {[0-9]+} to ensure that the id should be number if not found the data then pass the standard 404 message to client

--- server/routes/expenses.ts
+++ server/routes/expenses.ts
@@ -11,13 +11,35 @@ const fakeExpense: TExpense[] = [
 
 export const expensesRoute = new Hono()
     .get("/", (c) => {
-        return c.json({ success: true, data: fakeExpense });
+        try {
+            return c.json({ success: true, data: fakeExpense });
+        } catch (error) {
+            return c.json({
+                success: false,
+                message: "Internal Server Error!",
+                error: error instanceof Error ? error.message : "Unexpected Error."
+            }, 500);
+        }
     })
     .post("/", createExpenseValidator, async (c) => {
         try {
             const data = c.req.valid("json");
             fakeExpense.push({ id: fakeExpense.length + 1, ...data });
             return c.json({ success: true, data }, 201);
+        } catch (error) {
+            return c.json({
+                success: false,
+                message: "Internal Server Error!",
+                error: error instanceof Error ? error.message : "Unexpected Error."
+            }, 500);
+        }
+    })
+    .get("/:id{[0-9]+}", (c) => {
+        try {
+            const id = Number.parseInt(c.req.param("id"));
+            const data = fakeExpense.find(expense => expense.id === id);
+            if (!data) return c.json({ success: false, message: `Resource not found with id ${id}` }, 404);
+            return c.json({ success: true, data });
         } catch (error) {
             return c.json({
                 success: false,
