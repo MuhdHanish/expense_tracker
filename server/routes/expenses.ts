d62b7b74ccdcb1a8332f9c9684119f378695eb04
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-27 14:41:58 +0530

    /api/expense -> post handler added //fake data comment added try catch block and handled the error get the body from the req and validate it if body is there then return the body as json to user in response

--- server/routes/expenses.ts
+++ server/routes/expenses.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import type { TExpense } from "../types";
 
+// fake data
 const fakeExpense: TExpense[] = [
     { id: 1, title: "Groceries", amount: 150.75 },
     { id: 2, title: "Rent", amount: 1200.00 },
@@ -11,6 +12,18 @@ export const expensesRoute = new Hono()
     .get("/", (c) => {
         return c.json({ expenses: fakeExpense });
     })
-    .post("/", (c) => {
-        return c.json({});
+    .post("/", async (c) => {
+        try {
+            const body = await c.req.json();
+            if (!body || Object.keys(body).length === 0) {
+                return c.json({ message: "Invalid Data!" }, 400);
+            }
+            return c.json(body);
+        } catch (error) {
+            if (error instanceof SyntaxError) return c.json({ message: "Invalid JSON input!" }, 400);
+            else return c.json({
+                message: "Internal Server Error!",
+                error: error instanceof Error ? error.message : "Unexpected Error."
+            }, 500);
+        }
     });
