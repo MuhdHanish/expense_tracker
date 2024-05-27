92c49a71c8580e36ac66d9d074ded6f840189d73
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-27 13:48:40 +0530

    created a simple server with bun

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+const port = process.env.PORT || 8000;
+const hostname = process.env.HOST || '127.0.0.1';
+Bun.serve({
+    port,
+    hostname,
+    fetch(req) {
+        return new Response("Hello from bun server!");
+    }
+});
+console.log(`server running on http://${hostname}:${port}`);
