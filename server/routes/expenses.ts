6d2f2b238f0cda8b9dd6f12ac587a0b54f790f05
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-27 15:45:27 +0530

    /api/expense/:id delete handler check if already in the fake data using index if index -1 then pass standerd 404 if already there then splice the fake data and retrun the removed one in the reponse with success: true

--- server/routes/expenses.ts
+++ server/routes/expenses.ts
@@ -47,4 +47,19 @@ export const expensesRoute = new Hono()
                 error: error instanceof Error ? error.message : "Unexpected Error."
             }, 500);
         }
+    })
+    .delete("/:id{[0-9]+}", (c) => {
+        try {
+            const id = Number.parseInt(c.req.param("id"));
+            const index = fakeExpense.findIndex(expense => expense.id === id);
+            if (index === -1) return c.json({ success: false, message: `Resource not found with id ${id}` }, 404);
+            const data = fakeExpense.splice(index, 1)[0];
+            return c.json({ success: true, data });
+        } catch (error) {
+            return c.json({
+                success: false,
+                message: "Internal Server Error!",
+                error: error instanceof Error ? error.message : "Unexpected Error."
+            }, 500);
+        }
     });
