65aa23119f8a07f4ffaa6973e6da85f7f5d18240
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-27 18:08:04 +0530

    serve static files in the backend url http://localhost:8000 -> will show the client side anything will not handled by api show the static file (frontend)

--- server/app.ts
+++ server/app.ts
@@ -1,15 +1,18 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
+import { serveStatic } from "hono/bun";
 
-// routes
+// Routes
 import { expensesRoute } from "./routes";
 
 const app = new Hono();
 
 app.use(logger());
 
-app.get("/", (c) => c.json({ success: true, message: "Hello from Server!" }));
-
 app.route("/api/expenses", expensesRoute);
+
+// Serve static files (frontend) for unmatched routes
+app.get("*", serveStatic({ root: "../client/dist" }));
+app.get("*", serveStatic({ path: "../client/dist/index.html" }));
 
 export default app;
