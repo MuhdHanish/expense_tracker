1c4e26c104b8ff387814a03cb0f48e389f412961
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-27 17:49:31 +0530

    vite proxy requesting the resource to server using same origin http://localhost:5173/api -> http://localhost:8000/api

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,4 +10,12 @@ export default defineConfig({
       "@": path.resolve(import.meta.dirname, "./src"),
     },
   },
+  server: {
+    proxy: {
+      "/api": {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true
+      }
+    }
+  }
 });
