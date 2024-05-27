1a6cdfab5bb60a25fdbd57e1c3bf513142a69635
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-27 13:53:19 +0530

    used hono app in bun server

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,10 @@
+import app from "./app";
+
 const port = process.env.PORT || 8000;
 const hostname = process.env.HOST || '127.0.0.1';
 Bun.serve({
     port,
     hostname,
-    fetch(req) {
-        return new Response("Hello from bun server!");
-    }
+    fetch: app.fetch
 });
 console.log(`server running on http://${hostname}:${port}`);
