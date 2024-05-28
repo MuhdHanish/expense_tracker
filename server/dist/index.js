9897aa2cb5269d20b7167be76c8acf77cd51d8e8
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-28 09:10:45 +0530

    removed dist and src removed vercel.json

--- server/dist/index.js
+++
@@ -1,9 +0,0 @@
-import app from "./app";
-const port = process.env.PORT || 3000;
-const hostname = process.env.HOST || "localhost";
-Bun.serve({
-    port,
-    hostname,
-    fetch: app.fetch,
-});
-console.log(`server running on http://${hostname}:${port}`);
