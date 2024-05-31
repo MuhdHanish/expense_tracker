bda4788127ec257eb959692410d750852c163125
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-31 20:44:56 +0530

    implemented trpc using hono rpc

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,21 @@
 import { useEffect, useState } from "react";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
+// Hono client
+import { hc } from 'hono/client';
+
+// Type of Base API Route
+import { type ApiRoutes } from "../../server/app";
+
+// Client object
+const client = hc<ApiRoutes>("/");
+
 function App() {
   const [totalSpent, setTotalSpent] = useState(0);
   useEffect(() => { 
     async function fetchTotal() {
-      const response = await fetch("/api/expenses/total-spent");
+      const response = await client.api.expenses["total-spent"].$get();
+      if (!response.ok) throw new Error("The server may be experiencing issues. Please try again later.");
       const { data } = await response.json();
       const { total } = data;
       setTotalSpent(total);
