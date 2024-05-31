360fd15715ca75c90aab9a5a224bde32fecbf726
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-31 20:50:07 +0530

    moved the client new file and used here

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,12 @@
+import { api } from "@/lib/api";
 import { useEffect, useState } from "react";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-
-// Hono client
-import { hc } from 'hono/client';
-
-// Type of Base API Route
-import { type ApiRoutes } from "../../server/app";
-
-// Client object
-const client = hc<ApiRoutes>("/");
 
 function App() {
   const [totalSpent, setTotalSpent] = useState(0);
   useEffect(() => { 
     async function fetchTotal() {
-      const response = await client.api.expenses["total-spent"].$get();
+      const response = await api.expenses["total-spent"].$get();
       if (!response.ok) throw new Error("The server may be experiencing issues. Please try again later.");
       const { data } = await response.json();
       const { total } = data;
