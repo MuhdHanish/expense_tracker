2d43f553abf8a28150222d1be6a5c3873a12cb9f
MuhdHanish <muhammedhanish11@gmail.com>
2024-06-01 21:07:33 +0530

    used getProfileQueryOptions in useQuery hook

--- client/src/routes/profile.tsx
+++ client/src/routes/profile.tsx
@@ -1,4 +1,4 @@
-import { api } from "@/lib";
+import { getProfileQueryOptions } from "@/lib";
 import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 
@@ -6,18 +6,8 @@ export const Route = createFileRoute('/profile')({
     component: Profile
 });
 
-async function getProfile() {
-    const response = await api.auth["profile"].$get();
-    if (!response.ok) {
-        if (response.status >= 500) throw new Error("The server may be experiencing issues, Please try again later.");
-        else throw new Error("Not logged in.");
-    } 
-    const { data } = await response.json();
-    return data;
-};
-
 function Profile() {
-    const { data, error, isPending } = useQuery({ queryKey: ["get-profile"], queryFn: getProfile });
+    const { data, error, isPending } = useQuery(getProfileQueryOptions);
     if (isPending) return "Loading";
     if (error) return error.message;
 
