b66f4a4aa26c04c9aee45b50d0bce6a8e952ba8d
MuhdHanish <muhammedhanish11@gmail.com>
2024-06-01 09:31:58 +0530

    created navbar component

---
+++ client/src/components/navbar.tsx
@@ -0,0 +1,20 @@
+import { Link } from "@tanstack/react-router";
+
+export function NavBar() {
+    return (
+        <div className="p-2 flex gap-2">
+            <Link to="/" className="[&.active]:font-bold">
+                Home
+            </Link>{" "}
+            <Link to="/about" className="[&.active]:font-bold">
+                About
+            </Link>
+            <Link to="/expenses" className="[&.active]:font-bold">
+                Expenses
+            </Link>
+            <Link to="/create-expense" className="[&.active]:font-bold">
+                Create Expense
+            </Link>
+        </div>
+    );
+};
