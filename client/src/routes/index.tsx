5939e7eb2b53d8fe571a7ea6324bfecd1da34e53
MuhdHanish <muhammedhanish11@gmail.com>
2024-06-01 06:50:14 +0530

    moved App.tsx to index.tsx & removed App .tsx & .css files

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,13 +1,30 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { api } from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
+import { createFileRoute } from "@tanstack/react-router";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
 export const Route = createFileRoute('/')({
     component: Index,
 });
 
+async function getTotalSpent() {
+    const response = await api.expenses["total-spent"].$get();
+    if (!response.ok) throw new Error("The server may be experiencing issues, Please try again later.");
+    const { data } = await response.json();
+    return data;
+};
+
 function Index() {
+    const { data, error, isPending } = useQuery({ queryKey: ["get-total-spent"], queryFn: getTotalSpent });
+    if (error) return "An error has occured: " + error.message;
+
     return (
-        <div className="p-2">
-            <h3>Welcome Home!</h3>
-        </div>
-    )
+        <Card className="w-[350px] m-auto">
+            <CardHeader>
+                <CardTitle>Total Spent</CardTitle>
+                <CardDescription>The total amount you've spent</CardDescription>
+            </CardHeader>
+            <CardContent>{isPending ? "..." : data?.total}</CardContent>
+        </Card>
+    );
 };
