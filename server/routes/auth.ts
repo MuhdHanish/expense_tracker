56b956466280603d969b674f77836a37054116b7
MuhdHanish <muhammedhanish11@gmail.com>
2024-06-01 12:58:58 +0530

    passed c: Context parament to sessionManager

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -4,11 +4,11 @@ import { kindeClient, sessionManager } from "../kinde";
 export const authRoute = new Hono()
     // Post login
     .post("/login", async (c) => {
-        const loginUrl = await kindeClient.login(sessionManager);
+        const loginUrl = await kindeClient.login(sessionManager(c));
         return c.redirect(loginUrl.toString());
     })
     // Post register
     .post("/register", async (c) => {
-        const registerUrl = await kindeClient.register(sessionManager);
+        const registerUrl = await kindeClient.register(sessionManager(c));
         return c.redirect(registerUrl.toString());
     });
