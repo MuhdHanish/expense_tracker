2f79c2130dd4aac54b902588f6b5eda7b0a540fa
MuhdHanish <muhammedhanish11@gmail.com>
2024-06-01 14:11:04 +0530

    authentication create get /api/me to get the current user created get /api/login & /api/register created get /api/callback to redirect created get /api/logout

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -2,13 +2,74 @@ import { Hono } from "hono";
 import { kindeClient, sessionManager } from "../kinde";
 
 export const authRoute = new Hono()
-    // Post login
-    .post("/login", async (c) => {
-        const loginUrl = await kindeClient.login(sessionManager(c));
-        return c.redirect(loginUrl.toString());
+    // Get me, to check authentication status & pass the user
+    .get("/me", async (c) => {
+        try {
+            const isAuthenticated = await kindeClient.isAuthenticated(sessionManager(c));
+            if (!isAuthenticated) return c.json({ success: false, message: "Not Authenticated", data: { isAuthenticated } }, 401);
+            else {
+                const user = await kindeClient.getUserProfile(sessionManager(c));
+                return c.json({ success: true, data: { isAuthenticated, user } }, 200);
+            }
+        } catch (error) {
+            return c.json({
+                success: false,
+                message: "Internal Server Error!",
+                error: error instanceof Error ? error.message : "Unexpected Error."
+            }, 500);
+        }
     })
-    // Post register
-    .post("/register", async (c) => {
-        const registerUrl = await kindeClient.register(sessionManager(c));
-        return c.redirect(registerUrl.toString());
+    // Get login
+    .get("/login", async (c) => {
+        try {
+            const loginUrl = await kindeClient.login(sessionManager(c));
+            return c.redirect(loginUrl.toString());
+        } catch (error) {
+            return c.json({
+                success: false,
+                message: "Internal Server Error!",
+                error: error instanceof Error ? error.message : "Unexpected Error."
+            }, 500);
+        }
+    })
+    // Get register
+    .get("/register", async (c) => {
+        try {
+            const registerUrl = await kindeClient.register(sessionManager(c));
+            return c.redirect(registerUrl.toString());
+        } catch (error) {
+            return c.json({
+                success: false,
+                message: "Internal Server Error!",
+                error: error instanceof Error ? error.message : "Unexpected Error."
+            }, 500);
+        }
+    })
+    // Get callback
+    .get("/callback", async (c) => {
+        // get called every time we login or register
+        try {
+            const url = new URL(c.req.url);
+            await kindeClient.handleRedirectToApp(sessionManager(c), url);
+            return c.redirect("/");
+        } catch (error) {
+            return c.json({
+                success: false,
+                message: "Internal Server Error!",
+                error: error instanceof Error ? error.message : "Unexpected Error."
+            }, 500);
+        }
+    })
+    // Get logout
+    .get("/logout", async (c) => {
+        try {
+            const logoutUrl = await kindeClient.logout(sessionManager(c));
+            return c.redirect(logoutUrl.toString());
+        } catch (error) {
+            return c.json({
+                success: false,
+                message: "Internal Server Error!",
+                error: error instanceof Error ? error.message : "Unexpected Error."
+            }, 500);
+        }
     });
