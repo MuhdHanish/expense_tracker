fcce515a46fd9b091fbcb957dd1d7bced509aef5
MuhdHanish <muhammedhanish11@gmail.com>
2024-06-01 20:53:20 +0530

    profile get the reponse form the api checked the status code if code >= 500 then show the normal error else show the not logged in message destructured the datas from data object displayed user give_name & family_name

--- client/src/routes/profile.tsx
+++ client/src/routes/profile.tsx
@@ -1,21 +1,28 @@
 import { api } from "@/lib";
 import { useQuery } from "@tanstack/react-query";
-import { createFileRoute } from "@tanstack/react-router"
+import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute('/profile')({
     component: Profile
 });
 
 async function getProfile() {
-    const response = await api.auth["me"].$get();
-    if (!response.ok) throw new Error("The server may be experiencing issues, Please try again later.");
+    const response = await api.auth["profile"].$get();
+    if (!response.ok) {
+        if (response.status >= 500) throw new Error("The server may be experiencing issues, Please try again later.");
+        else throw new Error("Not logged in.");
+    } 
     const { data } = await response.json();
     return data;
 };
 
 function Profile() {
-    const { data, error, isPending } = useQuery({ queryKey: ["get-total-spent"], queryFn: getProfile });
-    if (error) return "An error has occured: " + error.message;
+    const { data, error, isPending } = useQuery({ queryKey: ["get-profile"], queryFn: getProfile });
+    if (isPending) return "Loading";
+    if (error) return error.message;
 
-    return <div className="p-2">Hello /profile!</div>
+    const { user } = data;
+    const { given_name, family_name } = user;
+
+    return <div className="p-2">Hello {given_name} {family_name} !</div>
 };
