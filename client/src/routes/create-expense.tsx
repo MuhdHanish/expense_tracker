9a1ce49cae6c438ab5d2f8cf0d52e1b3516380c4
MuhdHanish <muhammedhanish11@gmail.com>
2024-06-01 09:31:45 +0530

    formated code

--- client/src/routes/create-expense.tsx
+++ client/src/routes/create-expense.tsx
@@ -2,7 +2,7 @@ import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/create-expense')({
     component: CreateExpense
-})
+});
 
 function CreateExpense() {
     return (
--- client/src/routes/expenses.tsx
+++ client/src/routes/expenses.tsx
@@ -2,7 +2,7 @@ import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/expenses')({
     component: Expenses
-})
+});
 
 function Expenses() {
     return (
