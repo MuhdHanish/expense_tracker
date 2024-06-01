ccfe94d090991da8a31c8136328252dca8797989
MuhdHanish <muhammedhanish11@gmail.com>
2024-06-01 13:45:49 +0530

    remove /auth from auth route

--- server/app.ts
+++ server/app.ts
@@ -13,7 +13,7 @@ app.use("*", logger());
 const apiRoute = app.basePath('/api')
     // Sub routes
     // Auth
-    .route("/auth", authRoute)
+    .route("/", authRoute)
     // Expenses
     .route("/expenses", expensesRoute);
 
