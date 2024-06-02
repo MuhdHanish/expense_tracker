eb64bdf0f415727b5168ef87c8a54d54db4687ba
MuhdHanish <muhammedhanish11@gmail.com>
2024-06-02 13:09:35 +0530

    formated code

--- server/middlewares/auth.ts
+++ server/middlewares/auth.ts
@@ -12,7 +12,7 @@ type Env = {
 export const authMiddleware = createMiddleware<Env>(async (c, next) => {
     try {
         const isAuthenticated = await kindeClient.isAuthenticated(sessionManager(c));
-        if (!isAuthenticated) return c.json({ success: false, message: "Unauthorized: You need to log in to access this resource", data: { isAuthenticated } }, 401);
+        if (!isAuthenticated) return c.json({ success: false, message: "Unauthorized: You need to log in to access this resource.", data: { isAuthenticated } }, 401);
         else {
             const user = await kindeClient.getUserProfile(sessionManager(c));
             c.set("isAuthenticated", true);
