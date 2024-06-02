37e062e0026ad1ea21735c122a0482e63b71b718
MuhdHanish <muhammedhanish11@gmail.com>
2024-06-02 10:23:26 +0530

    created a basic expenses table schema using drizzle orm pg-core

---
+++ server/database/schema/expenses.ts
@@ -0,0 +1,12 @@
+import { index, numeric, pgTable, serial, text } from 'drizzle-orm/pg-core';
+
+export const expenses = pgTable("expenses", {
+    id: serial("id").primaryKey(),
+    userId: text("user_id").notNull(),
+    title: text("title").notNull(),
+    amount: numeric("amount", { precision: 12, scale: 2 })
+}, (expenses) => {
+    return {
+        userIdIndex: index("user_id_index").on(expenses.userId),
+    }
+});
