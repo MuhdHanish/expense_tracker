0eb03563b976538b22efb70c6cd32b522fa97d15
MuhdHanish <muhammedhanish11@gmail.com>
2024-06-02 14:28:45 +0530

    refracted the routes with utils

--- server/routes/expenses.ts
+++ server/routes/expenses.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import type { TExpense } from "../types";
+import { CustomError, catchHandler, getUserId } from "../utils";
 
 // Middlewares
 import { authMiddleware } from "../middlewares";
@@ -18,110 +18,77 @@ export const expensesRoute = new Hono()
     // Get expenses
     .get("/", authMiddleware, async (c) => {
         try {
-            const { user } = c.var;
-            const { id } = user;
+            const userId = getUserId(c);
             const expenses = await database
                 .select()
                 .from(expensesTable)
-                .where(eq(expensesTable.userId, id))
+                .where(eq(expensesTable.userId, userId))
                 .orderBy(desc(expensesTable.createdAt))
                 .limit(10);
             return c.json({ success: true, data: { expenses } });
         } catch (error) {
-            console.error('Error fetching expenses:', error);
-            return c.json({
-                success: false,
-                message: "Internal Server Error",
-                error: error instanceof Error ? error.message : "Unexpected Error"
-            }, 500);
+            catchHandler(c, error);
         }
     })
     // Post expense
     .post("/", authMiddleware, createExpenseValidator, async (c) => {
         try {
-            const { user } = c.var;
-            const { id: userId } = user;
+            const userId = getUserId(c);
             const expenseDTO = c.req.valid("json");
             const expense = await database
                 .insert(expensesTable)
-                .values({
-                    userId,
-                    ...expenseDTO,
-                })
+                .values({userId, ...expenseDTO,})
                 .returning()
                 .then(result => result[0]);
             return c.json({ success: true, data: { expense } }, 201);
         } catch (error) {
-            console.error('Error inserting expense:', error);
-            return c.json({
-                success: false,
-                message: "Internal Server Error",
-                error: error instanceof Error ? error.message : "Unexpected Error"
-            }, 500);
+            catchHandler(c, error);
         }
     })
     // Get expense by id
     .get("/:id{[0-9]+}", authMiddleware, async (c) => {
         try {
-            const { user } = c.var;
-            const { id: userId } = user;
+            const userId = getUserId(c);
             const id = Number.parseInt(c.req.param("id"));
             const expense = await database
                 .delete(expensesTable)
                 .where(and(eq(expensesTable.id, id), eq(expensesTable.userId, userId)))
                 .returning()
                 .then(result => result[0]);
-            if (!expense) return c.json({ success: false, message: `Expense with id ${id} not found for the current user` }, 404);
+            if (!expense) throw new CustomError(`Expense with id ${id} not found for the current user`, 404);
             return c.json({ success: true, data: { expense } });
         } catch (error) {
-            console.error('Error fetching expense by id:', error);
-            return c.json({
-                success: false,
-                message: "Internal Server Error",
-                error: error instanceof Error ? error.message : "Unexpected Error"
-            }, 500);
+            catchHandler(c, error);
         }
     })
     // Delete expense by id
     .delete("/:id{[0-9]+}", authMiddleware, async (c) => {
         try {
-            const { user } = c.var;
-            const { id: userId } = user;
+            const userId = getUserId(c);
             const id = Number.parseInt(c.req.param("id"));
             const expense = await database
                 .select()
                 .from(expensesTable)
                 .where(and(eq(expensesTable.id, id), eq(expensesTable.userId, userId)))
                 .then(result => result[0]);
-            if (!expense) return c.json({ success: false, message: `Expense with id ${id} not found for the current user` }, 404);
+            if (!expense) throw new CustomError(`Expense with id ${id} not found for the current user`, 404);
             return c.json({ success: true, data: { expense } });
         } catch (error) {
-            console.error('Error deleting expense by id:', error);
-            return c.json({
-                success: false,
-                message: "Internal Server Error",
-                error: error instanceof Error ? error.message : "Unexpected Error"
-            }, 500);
+            catchHandler(c, error);
         }
     })
     // Get total spent
     .get("/total-spent", authMiddleware, async (c) => {
         try {
-            const { user } = c.var;
-            const { id } = user;
+            const userId = getUserId(c);
             const { total } = await database
                 .select({ total: sum(expensesTable.amount) })
                 .from(expensesTable)
-                .where(eq(expensesTable.userId, id))
+                .where(eq(expensesTable.userId, userId))
                 .limit(1)
                 .then(result => result[0]);
             return c.json({ success: true, data: { total } });
         } catch (error) {
-            console.error('Error fetching total spent:', error);
-            return c.json({
-                success: false,
-                message: "Internal Server Error",
-                error: error instanceof Error ? error.message : "Unexpected Error"
-            }, 500);
+            catchHandler(c, error);
         }
     });
