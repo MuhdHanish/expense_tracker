aacdd7d133ac2e770c187645c72dcef8e7684f19
MuhdHanish <muhammedhanish11@gmail.com>
2024-06-02 12:42:30 +0530

    added timestamp filed

--- server/database/schema/expenses.ts
+++ server/database/schema/expenses.ts
@@ -1,10 +1,11 @@
-import { index, numeric, pgTable, serial, text } from "drizzle-orm/pg-core";
+import { index, numeric, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 
 export const expenses = pgTable("expenses", {
     id: serial("id").primaryKey(),
     userId: text("user_id").notNull(),
     title: text("title").notNull(),
-    amount: numeric("amount", { precision: 12, scale: 2 })
+    amount: numeric("amount", { precision: 12, scale: 2 }),
+    createdAt: timestamp("created_at").defaultNow()
 }, (expenses) => {
     return {
         userIdIndex: index("user_id_index").on(expenses.userId),
