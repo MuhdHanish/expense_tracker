fb47c2c25d981a28c567d0abbb02768ef4b514c7
MuhdHanish <muhammedhanish11@gmail.com>
2024-06-02 11:47:51 +0530

    added comments & the end func to close the connect

--- migrate.ts
+++ migrate.ts
@@ -4,5 +4,11 @@ import { migrate } from "drizzle-orm/postgres-js/migrator";
 
 // for migrations
 const migrationClient = postgres(process.env.DATABASE_URL!, { max: 1 });
+
+// This will run migrations on the database, skipping the ones already applied
 await migrate(drizzle(migrationClient), { migrationsFolder: "./drizzle" });
 console.log(`Migration completed 🚀`);
+
+
+// Don't forget to close the connection, otherwise the script will hang
+await migrationClient.end();
