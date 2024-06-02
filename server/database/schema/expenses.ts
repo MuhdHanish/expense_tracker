8e4223bbe3aab4d7f24e48c2c630a4c59831fa4e
MuhdHanish <muhammedhanish11@gmail.com>
2024-06-02 17:11:01 +0530

    created insert & select Expense Schema, to give more controller over the schema

--- server/database/schema/expenses.ts
+++ server/database/schema/expenses.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import { index, numeric, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 
 export const expenses = pgTable("expenses", {
@@ -11,3 +13,16 @@ export const expenses = pgTable("expenses", {
         userIdIndex: index("user_id_index").on(expenses.userId),
     }
 });
+
+
+// Schema for inserting a expenses - can be used to validate API requests
+export const insertExpensesSchema = createInsertSchema(expenses, {
+    title: z
+        .string()
+        .min(3, { message: 'Title must be at least 3 characters' }),
+    amount: z
+        .string()
+        .regex(/^([0-9]\d*)(\.\d{1,2})?$/, { message: 'Amount must be a valid monetary value' })
+});
+// Schema for selecting a expenses - can be used to validate API responses
+export const selectExpensesSchema = createSelectSchema(expenses);
