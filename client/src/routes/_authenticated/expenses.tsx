7036ede09ecad117673455adc39602b0cb432361
MuhdHanish <muhammedhanish11@gmail.com>
2024-06-02 21:33:49 +0530

    added the date in table

--- client/src/routes/_authenticated/expenses.tsx
+++ client/src/routes/_authenticated/expenses.tsx
@@ -38,6 +38,7 @@ function Expenses() {
                         <TableHead className="w-[100px]">ID</TableHead>
                         <TableHead>Title</TableHead>
                         <TableHead>Amount</TableHead>
+                        <TableHead>Date</TableHead>
                     </TableRow>
                 </TableHeader>
                 <TableBody>
@@ -48,6 +49,7 @@ function Expenses() {
                                 <TableCell><Skeleton className="h-4"/></TableCell>
                                 <TableCell><Skeleton className="h-4"/></TableCell>
                                 <TableCell><Skeleton className="h-4"/></TableCell>
+                                <TableCell><Skeleton className="h-4"/></TableCell>
                             </TableRow>
                         ))
                         : data?.expenses?.map((expense) => (
@@ -55,6 +57,7 @@ function Expenses() {
                             <TableCell className="font-medium">{expense?.id}</TableCell>
                             <TableCell>{expense?.title}</TableCell>
                             <TableCell>{expense?.amount}</TableCell>
+                            <TableCell>{expense?.date}</TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
