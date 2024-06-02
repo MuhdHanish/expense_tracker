baede01bd24b2e8798dc42985929ae92525f7840
MuhdHanish <muhammedhanish11@gmail.com>
2024-06-02 21:53:09 +0530

    added padding and increased teh gap

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -2,9 +2,9 @@ import { Link } from "@tanstack/react-router";
 
 export function NavBar() {
     return (
-        <div className="p-2 flex justify-between max-w-2xl m-auto items-baseline">
+        <div className="p-3 flex justify-between max-w-2xl m-auto items-baseline">
             <Link to="/"><h1 className="text-2xl font-bold">Expense Tracker</h1></Link>
-            <div className="flex gap-5">
+            <div className="flex gap-7">
                 <Link to="/expenses" className="[&.active]:font-bold">
                     Expenses
                 </Link>
