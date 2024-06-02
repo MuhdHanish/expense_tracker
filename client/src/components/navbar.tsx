e9cb7c5308ee16bb96734235348c57663be91f2f
MuhdHanish <muhammedhanish11@gmail.com>
2024-06-02 21:33:02 +0530

    add new layout for navbar & removed about route

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -2,22 +2,19 @@ import { Link } from "@tanstack/react-router";
 
 export function NavBar() {
     return (
-        <div className="p-2 flex gap-2 max-w-2xl m-auto">
-            <Link to="/" className="[&.active]:font-bold">
-                Home
-            </Link>{" "}
-            <Link to="/about" className="[&.active]:font-bold">
-                About
-            </Link>
-            <Link to="/expenses" className="[&.active]:font-bold">
-                Expenses
-            </Link>
-            <Link to="/create-expense" className="[&.active]:font-bold">
-                Create
-            </Link>
-            <Link to="/profile" className="[&.active]:font-bold">
-                Profile
-            </Link>
+        <div className="p-2 flex justify-between max-w-2xl m-auto items-baseline">
+            <Link to="/"><h1 className="text-2xl font-bold">Expense Tracker</h1></Link>
+            <div className="flex gap-5">
+                <Link to="/expenses" className="[&.active]:font-bold">
+                    Expenses
+                </Link>
+                <Link to="/create-expense" className="[&.active]:font-bold">
+                    Create
+                </Link>
+                <Link to="/profile" className="[&.active]:font-bold">
+                    Profile
+                </Link>
+            </div>
         </div>
     );
 };
