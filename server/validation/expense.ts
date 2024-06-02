7640abf9581d5963c06f7458412fca137c146da2
MuhdHanish <muhammedhanish11@gmail.com>
2024-06-02 17:11:54 +0530

    used the insertExpesnse schema to create the createExpense schema using for api validator and client form

--- server/validation/expense.ts
+++ server/validation/expense.ts
@@ -1,20 +1,11 @@
-import { z } from "zod";
 import { zValidator } from "@hono/zod-validator";
+import { insertExpensesSchema } from "../database/schema/expenses";
 
-// Expense schema
-export const expenseSchema = z.object({
-    id: z.number().int().positive().min(1),
-    title: z
-        .string()
-        .min(3, { message: 'Title must be at least 3 characters' })
-        .max(50, { message: 'Title must not exceed 50 characters' }),
-    amount: z
-        .string()
-        .regex(/^([0-9]\d*)(\.\d{1,2})?$/, { message: 'Amount must be a valid number' })
+// Create Expense schema
+export const createExpenseSchema = insertExpensesSchema.omit({
+    userId: true,
+    createdAt: true
 });
-
-// Create expense schema
-export const createExpenseSchema = expenseSchema.omit({ id: true });
 
 // Create expense validator
 export const createExpenseValidator = zValidator("json", createExpenseSchema);
