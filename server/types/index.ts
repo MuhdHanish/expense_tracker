968000cb590fec58ffe037c60d6cf48c6e38b5b9
MuhdHanish <muhammedhanish11@gmail.com>
2024-06-06 21:18:28 +0530

    created pagination type and exported

--- server/types/index.ts
+++ server/types/index.ts
@@ -3,3 +3,10 @@ import type { createExpenseSchema } from "../validation";
 
 // Zod validation expense schema used to create type of expense
 export type TExpense = z.infer<typeof createExpenseSchema>;
+
+export type TPagination = {
+    prev: number | null;
+    next: number | null;
+    currentPage: number;
+    totalPages: number
+}
