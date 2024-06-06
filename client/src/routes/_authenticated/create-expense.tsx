c44650a078e1364ae9a3ebcc26336aba930b5fdd
MuhdHanish <muhammedhanish11@gmail.com>
2024-06-06 11:10:37 +0530

    create expense get the existing expenses from the cached navigate to /expenses page set the newly created expense in to loading create expense query options moved the crate expense to utils used the created expense function and get the new expense set the new created expense in to existing expenses if success show toast if error show error toast in finally set the laoding create expense data to {} change the form field errors to destructive

--- client/src/routes/_authenticated/create-expense.tsx
+++ client/src/routes/_authenticated/create-expense.tsx
@@ -1,10 +1,12 @@
-import { api } from "@/lib";
+import { useQueryClient } from "@tanstack/react-query";
+import { createExpense, formattedDateDescription, getAllExpensesQueryOptions, loadingCreateExpenseQueryOptions } from "@/utils";
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
 
 // Loader icon
 import { Loader2 } from "lucide-react";
 
 // UI components
+import { toast } from "sonner";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useForm } from "@tanstack/react-form";
@@ -17,11 +19,12 @@ import { createExpenseSchema } from "@server/validation";
 // Zod validator from tanstack-form
 import { zodValidator } from "@tanstack/zod-form-adapter";
 
-export const Route = createFileRoute('/_authenticated/create-expense')({
-    component: CreateExpense
+export const Route = createFileRoute("/_authenticated/create-expense")({
+    component: CreateExpense,
 });
 
 function CreateExpense() {
+    const queryClient = useQueryClient();
     const naviagte = useNavigate();
     const form = useForm({
         validatorAdapter: zodValidator,
@@ -31,22 +34,40 @@ function CreateExpense() {
             date: new Date().toISOString(),
         },
         onSubmit: async ({ value }) => {
-            const response = await api.expenses.$post({ json: value });
-            if (!response.ok) throw new Error("The server may be experiencing issues, Please try again later.");
+            const { expenses: existingExpenses } = await queryClient.ensureQueryData(
+                getAllExpensesQueryOptions
+            );
             naviagte({ to: "/expenses" });
+            queryClient.setQueryData(loadingCreateExpenseQueryOptions.queryKey, { expense: value });
+            try {
+                const { expense } = await createExpense({ value });
+                queryClient.setQueryData(getAllExpensesQueryOptions.queryKey, {
+                    ...existingExpenses,
+                    expenses: [expense, ...existingExpenses],
+                });
+                const description = formattedDateDescription();
+                toast.message(`Expense has been created successfully: ${expense?.id}`, { description });
+            } catch (error) {
+                toast.error(
+                    `Failed to create a new expense`,
+                    { description: error instanceof Error ? error.message : 'An error occurred' }
+                );
+            } finally {
+                queryClient.setQueryData(loadingCreateExpenseQueryOptions.queryKey, {});
+            }
         },
-    })
+    });
 
     return (
         <div className="p-2 max-w-xl m-auto flex flex-col gap-y-4">
             <h2 className="font-bold">Create Expense</h2>
-            <form className="flex flex-col gap-y-3.5"
+            <form
+                className="flex flex-col gap-y-3.5"
                 onSubmit={(e) => {
-                    e.preventDefault()
-                    e.stopPropagation()
-                    form.handleSubmit()
+                    e.preventDefault();
+                    e.stopPropagation();
+                    form.handleSubmit();
                 }}
-
             >
                 <form.Field
                     name="title"
@@ -66,10 +87,12 @@ function CreateExpense() {
                                     onChange={(e) => field.handleChange(e.target.value)}
                                 />
                                 {field.state.meta.touchedErrors ? (
-                                    <em className="text-xs text-red-600">{field.state.meta.touchedErrors}</em>
+                                    <em className="text-xs text-destructive">
+                                        {field.state.meta.touchedErrors}
+                                    </em>
                                 ) : null}
                             </div>
-                        )
+                        );
                     }}
                 />
                 <form.Field
@@ -92,10 +115,12 @@ function CreateExpense() {
                                     onChange={(e) => field.handleChange(e.target.value)}
                                 />
                                 {field.state.meta.touchedErrors ? (
-                                    <em className="text-xs text-red-600">{field.state.meta.touchedErrors}</em>
+                                    <em className="text-xs text-destructive">
+                                        {field.state.meta.touchedErrors}
+                                    </em>
                                 ) : null}
                             </div>
-                        )
+                        );
                     }}
                 />
                 <form.Field
@@ -110,30 +135,34 @@ function CreateExpense() {
                                 <Calendar
                                     mode="single"
                                     selected={new Date(field.state.value)}
-                                    onSelect={(date) => field.handleChange((date ?? new Date()).toISOString())}
+                                    onSelect={(date) =>
+                                        field.handleChange((date ?? new Date()).toISOString())
+                                    }
                                     className="rounded-md border"
                                 />
                                 {field.state.meta.touchedErrors ? (
                                     <em>{field.state.meta.touchedErrors}</em>
                                 ) : null}
                             </div>
-                        )
+                        );
                     }}
                 />
                 <form.Subscribe
                     selector={(state) => [state.canSubmit, state.isSubmitting]}
                     children={([canSubmit, isSubmitting]) => (
                         <Button className="mt-4" type="submit" disabled={!canSubmit}>
-                            {isSubmitting ?
+                            {isSubmitting ? (
                                 <>
-                                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                                Please wait
+                                    <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                                    Please wait
                                 </>
-                                : 'Submit'}
+                            ) : (
+                                "Submit"
+                            )}
                         </Button>
                     )}
                 />
             </form>
         </div>
     );
-};
+}
